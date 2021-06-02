d5955ddfaf90e282ec57273109318a9750cd3500
serginaAleksandra <sergina.al.al@gmail.com>
2021-06-02 13:39:56 +0700

    first commit

---
+++ lab13/Form1.Designer.cs
@@ -0,0 +1,292 @@
+﻿
+namespace lab13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea6 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.CustomLabel customLabel41 = new System.Windows.Forms.DataVisualization.Charting.CustomLabel();
+            System.Windows.Forms.DataVisualization.Charting.CustomLabel customLabel42 = new System.Windows.Forms.DataVisualization.Charting.CustomLabel();
+            System.Windows.Forms.DataVisualization.Charting.CustomLabel customLabel43 = new System.Windows.Forms.DataVisualization.Charting.CustomLabel();
+            System.Windows.Forms.DataVisualization.Charting.CustomLabel customLabel44 = new System.Windows.Forms.DataVisualization.Charting.CustomLabel();
+            System.Windows.Forms.DataVisualization.Charting.CustomLabel customLabel45 = new System.Windows.Forms.DataVisualization.Charting.CustomLabel();
+            System.Windows.Forms.DataVisualization.Charting.CustomLabel customLabel46 = new System.Windows.Forms.DataVisualization.Charting.CustomLabel();
+            System.Windows.Forms.DataVisualization.Charting.CustomLabel customLabel47 = new System.Windows.Forms.DataVisualization.Charting.CustomLabel();
+            System.Windows.Forms.DataVisualization.Charting.CustomLabel customLabel48 = new System.Windows.Forms.DataVisualization.Charting.CustomLabel();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend6 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series11 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series12 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Purple;
+            this.label1.Location = new System.Drawing.Point(59, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mean";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.LightGreen;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.ForeColor = System.Drawing.Color.DarkOrchid;
+            this.textBox1.Location = new System.Drawing.Point(145, 43);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(87, 28);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.LightGreen;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.ForeColor = System.Drawing.Color.DarkOrchid;
+            this.textBox2.Location = new System.Drawing.Point(145, 84);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(87, 28);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.Purple;
+            this.label2.Location = new System.Drawing.Point(59, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 22);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Variance";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.MediumPurple;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.Color.Honeydew;
+            this.button1.Location = new System.Drawing.Point(88, 171);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 39);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chart1
+            // 
+            this.chart1.BackColor = System.Drawing.Color.MintCream;
+            this.chart1.BorderlineColor = System.Drawing.Color.MintCream;
+            customLabel41.ForeColor = System.Drawing.Color.White;
+            customLabel41.FromPosition = 2D;
+            customLabel41.Text = "[-4;-3]";
+            customLabel42.FromPosition = 4D;
+            customLabel42.Text = "[-3;-2]";
+            customLabel43.FromPosition = 6D;
+            customLabel43.Text = "[-2;-1]";
+            customLabel44.FromPosition = 8D;
+            customLabel44.Text = "[-1;0]";
+            customLabel45.FromPosition = 10D;
+            customLabel45.Text = "[0;1]";
+            customLabel46.FromPosition = 12D;
+            customLabel46.Text = "[1;2]";
+            customLabel47.FromPosition = 14D;
+            customLabel47.Text = "[2;3]";
+            customLabel48.FromPosition = 16D;
+            customLabel48.Text = "[3;4]";
+            chartArea6.AxisX.CustomLabels.Add(customLabel41);
+            chartArea6.AxisX.CustomLabels.Add(customLabel42);
+            chartArea6.AxisX.CustomLabels.Add(customLabel43);
+            chartArea6.AxisX.CustomLabels.Add(customLabel44);
+            chartArea6.AxisX.CustomLabels.Add(customLabel45);
+            chartArea6.AxisX.CustomLabels.Add(customLabel46);
+            chartArea6.AxisX.CustomLabels.Add(customLabel47);
+            chartArea6.AxisX.CustomLabels.Add(customLabel48);
+            chartArea6.BackColor = System.Drawing.Color.MintCream;
+            chartArea6.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea6);
+            legend6.Name = "Legend1";
+            this.chart1.Legends.Add(legend6);
+            this.chart1.Location = new System.Drawing.Point(268, 12);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.SeaGreen;
+            series11.BorderColor = System.Drawing.Color.Transparent;
+            series11.ChartArea = "ChartArea1";
+            series11.CustomProperties = "MinPixelPointWidth=25, PointWidth=0.9, MaxPixelPointWidth=50";
+            series11.IsValueShownAsLabel = true;
+            series11.Legend = "Legend1";
+            series11.Name = "Series1";
+            series12.ChartArea = "ChartArea1";
+            series12.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series12.Legend = "Legend1";
+            series12.Name = "Series2";
+            this.chart1.Series.Add(series11);
+            this.chart1.Series.Add(series12);
+            this.chart1.Size = new System.Drawing.Size(843, 357);
+            this.chart1.TabIndex = 6;
+            this.chart1.Text = "chart1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.Color.Purple;
+            this.label4.Location = new System.Drawing.Point(333, 450);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(113, 22);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Chi-squared:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.ForeColor = System.Drawing.Color.Purple;
+            this.label5.Location = new System.Drawing.Point(333, 419);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(86, 22);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Variance:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.ForeColor = System.Drawing.Color.Purple;
+            this.label6.Location = new System.Drawing.Point(333, 388);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(82, 22);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Average:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.ForeColor = System.Drawing.Color.Purple;
+            this.label7.Location = new System.Drawing.Point(586, 450);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 22);
+            this.label7.TabIndex = 11;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.ForeColor = System.Drawing.Color.Purple;
+            this.label8.Location = new System.Drawing.Point(333, 488);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(55, 22);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Time:";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.MediumPurple;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.ForeColor = System.Drawing.Color.Honeydew;
+            this.button2.Location = new System.Drawing.Point(88, 216);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 39);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Start 2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.MediumPurple;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.ForeColor = System.Drawing.Color.Honeydew;
+            this.button3.Location = new System.Drawing.Point(88, 262);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(101, 39);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Start 3";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.ClientSize = new System.Drawing.Size(1123, 588);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ lab13/Form1.cs
@@ -0,0 +1,358 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+//using System.Security.Cryptography;
+
+namespace lab13
+{
+    public partial class Form1 : Form
+    {
+
+        Random rnd = new Random();
+
+        double[] pl = new double[8];
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Stopwatch sWatch = new Stopwatch();
+
+            double n = 1000;
+            double M = Convert.ToDouble(textBox1.Text); ;
+            double D = Convert.ToDouble(textBox2.Text);
+            double[] Statistics = new double[8];
+            double[] Frequency = new double[8];
+            double Ex = 0;
+            double Dx = 0;
+            int i = 0;
+
+            sWatch.Start();
+            while (i < n)
+            {
+                double a_sum = SUMMING();
+                Statistics[getIndex(a_sum)]++;
+                Ex += a_sum;
+                Dx += Math.Pow(a_sum, 2);
+                i++;
+            }
+            sWatch.Stop();
+
+            label8.Text = "Time: " + sWatch.ElapsedMilliseconds.ToString() + "ms";
+            Ex *= 1 / n;
+            Dx *= 1 / n;
+            Dx -= Math.Pow(Ex, 2);
+            Ex = Math.Abs(Ex);
+
+            double err_abs = Math.Abs(Ex - 0);
+            double err_var = Math.Abs(Dx - 1);
+
+            label6.Text = "Average: " + Math.Round(Ex, 2) + " (error = " + Math.Round(err_abs, 2) + ")";
+            label5.Text = "Variance: " + Math.Round(Dx, 2) + " (error = " + Math.Round(err_var, 2) + ")";
+
+            Initpl(M, D);
+            double chi_squared = 0;
+            double _chi = 0;
+            double l = -4;
+            for (int j = 0; j < 8; j++)
+            {
+                double p = (l + 1 - l) * pl[j];
+
+
+                _chi += Math.Pow(Statistics[j], 2) / (n * p);
+                l++;
+            }
+
+            chi_squared = _chi - n;
+
+
+            label4.Text = "Chi-squared: " + Math.Round(chi_squared, 2) + " < 20.09 is";
+            for (int j = 0; j < 8; j++)
+            {
+                Frequency[j] = Statistics[j] / n;
+
+                chart1.Series[0].Points.AddXY(j + 1, Frequency[j]);
+                chart1.Series[1].Points.AddXY(j + 1, pl[j]);
+            }
+
+            label7.Visible = true;
+            if (chi_squared < 20.09)
+            {
+                label7.Text = "true";
+                label7.ForeColor = Color.Green;
+            }
+            else
+            {
+                label7.Text = "false";
+                label7.ForeColor = Color.Red;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Stopwatch sWatch = new Stopwatch();
+
+            double n = 1000;
+            double M = Convert.ToDouble(textBox1.Text);
+            double D = Convert.ToDouble(textBox2.Text);
+            double[] Statistics = new double[8];
+            double[] Frequency = new double[8];
+            double Ex = 0;
+            double Dx = 0;
+            int i = 0;
+            sWatch.Start();
+            while (i < n)
+            {
+                double a_sum = SummingPLUS();
+                Statistics[getIndex(a_sum)]++;
+                Ex += a_sum;
+                Dx += Math.Pow(a_sum, 2);
+                i++;
+            }
+            sWatch.Stop();
+
+            label8.Text = "Time: " + sWatch.ElapsedMilliseconds.ToString() + "ms";
+            Ex *= 1 / n;
+            Dx *= 1 / n;
+            Dx -= Math.Pow(Ex, 2);
+            Ex = Math.Abs(Ex);
+            for (int j = 0; j < 8; j++)
+            {
+                Frequency[j] = Statistics[j] / n;
+
+                chart1.Series[0].Points.AddXY(j + 1, Frequency[j]);
+                chart1.Series[1].Points.AddXY(j + 1, pl[j]);
+            }
+
+            double err_abs = Math.Abs(Ex - 0);
+            double err_var = Math.Abs(Dx - 1);
+
+            label6.Text = "Average: " + Math.Round(Ex, 2) + " (error = " + Math.Round(err_abs, 2) + ")";
+            label5.Text = "Variance: " + Math.Round(Dx, 2) + " (error = " + Math.Round(err_var, 2) + ")";
+
+            Initpl(M, D);
+            double chi_squared = 0;
+            double _chi = 0;
+            double l = -4;
+            for (int j = 0; j < 8; j++)
+            {
+                double p = (l + 1 - l) * pl[j];
+
+
+                _chi += Math.Pow(Statistics[j], 2) / (n * p);
+                l++;
+            }
+
+            chi_squared = _chi - n;
+
+
+            label4.Text = "Chi-squared: " + Math.Round(chi_squared, 2) + " < 20,09 is";
+
+
+            label7.Visible = true;
+            if (chi_squared < 20.09)
+            {
+                label7.Text = "true";
+                label7.ForeColor = Color.Green;
+            }
+            else
+            {
+                label7.Text = "false";
+                label7.ForeColor = Color.Red;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Stopwatch sWatch = new Stopwatch();
+
+            double n = 1000;
+            double M = Convert.ToDouble(textBox1.Text);
+            double D = Convert.ToDouble(textBox2.Text);
+            double[] Statistics = new double[8];
+            double[] Frequency = new double[8];
+            double Ex = 0;
+            double Dx = 0;
+            int i = 0;
+            sWatch.Start();
+            while (i < n)
+            {
+                double a_sum = BoxMuller();
+                Statistics[getIndex(a_sum)]++;
+                Ex += a_sum;
+                Dx += Math.Pow(a_sum, 2);
+                i++;
+            }
+            sWatch.Stop();
+
+            label8.Text = "Time: " + sWatch.ElapsedMilliseconds.ToString() + "ms";
+            Ex *= 1 / n;
+            Dx *= 1 / n;
+            Dx -= Math.Pow(Ex, 2);
+            Ex = Math.Abs(Ex);
+
+            for (int j = 0; j < 8; j++)
+            {
+                Frequency[j] = Statistics[j] / n;
+
+                chart1.Series[0].Points.AddXY(j + 1, Frequency[j]);
+                chart1.Series[1].Points.AddXY(j + 1, pl[j]);
+            }
+
+            double err_abs = Math.Abs(Ex - 0);
+            double err_var = Math.Abs(Dx - 1);
+
+            label6.Text = "Average: " + Math.Round(Ex, 2) + " (error = " + Math.Round(err_abs, 2) + ")";
+            label5.Text = "Variance: " + Math.Round(Dx, 2) + " (error = " + Math.Round(err_var, 2) + ")";
+
+            Initpl(M, D);
+            double chi_squared = 0;
+            double _chi = 0;
+            double l = -4;
+            for (int j = 0; j < 8; j++)
+            {
+                double p = (l + 1 - l) * pl[j];
+
+
+                _chi += Math.Pow(Statistics[j], 2) / (n * p);
+                l++;
+            }
+
+            chi_squared = _chi - n;
+
+
+            label4.Text = "Chi-squared: " + Math.Round(chi_squared, 2) + " < 20.09 is";
+
+
+            label7.Visible = true;
+            if (chi_squared < 20.09)
+            {
+                label7.Text = "true";
+                label7.ForeColor = Color.Green;
+            }
+            else
+            {
+                label7.Text = "false";
+                label7.ForeColor = Color.Red;
+            }
+        }
+
+        private void Initpl(double M, double D)
+        {
+
+            pl[0] = 1 / (Math.Sqrt(D) * Math.Sqrt(2 * Math.PI)) * Math.Pow(Math.E, -(Math.Pow(-3.5 - M, 2) / (2 * D)));
+            pl[1] = 1 / (Math.Sqrt(D) * Math.Sqrt(2 * Math.PI)) * Math.Pow(Math.E, -(Math.Pow(-2.5 - M, 2) / (2 * D)));
+            pl[2] = 1 / (Math.Sqrt(D) * Math.Sqrt(2 * Math.PI)) * Math.Pow(Math.E, -(Math.Pow(-1.5 - M, 2) / (2 * D)));
+            pl[3] = 1 / (Math.Sqrt(D) * Math.Sqrt(2 * Math.PI)) * Math.Pow(Math.E, -(Math.Pow(-0.5 - M, 2) / (2 * D)));
+            pl[4] = 1 / (Math.Sqrt(D) * Math.Sqrt(2 * Math.PI)) * Math.Pow(Math.E, -(Math.Pow(0.5 - M, 2) / (2 * D)));
+            pl[5] = 1 / (Math.Sqrt(D) * Math.Sqrt(2 * Math.PI)) * Math.Pow(Math.E, -(Math.Pow(1.5 - M, 2) / (2 * D)));
+            pl[6] = 1 / (Math.Sqrt(D) * Math.Sqrt(2 * Math.PI)) * Math.Pow(Math.E, -(Math.Pow(2.5 - M, 2) / (2 * D)));
+            pl[7] = 1 / (Math.Sqrt(D) * Math.Sqrt(2 * Math.PI)) * Math.Pow(Math.E, -(Math.Pow(3.5 - M, 2) / (2 * D)));
+        }
+        private double SUMMING()
+        {
+            chart1.Series[0].Points.Clear();
+            chart1.Series[1].Points.Clear();
+
+            double a_sum = 0;
+
+            int j;
+
+            for (int i = 0; i < 12; i++)
+            {
+                a_sum += rnd.NextDouble();
+
+            }
+            a_sum -= 6;
+
+            return a_sum;
+
+        }
+
+        private double SummingPLUS()
+        {
+            chart1.Series[0].Points.Clear();
+            chart1.Series[1].Points.Clear();
+
+            double sup = SUMMING();
+
+            double res = sup + (1 / 240 * (Math.Pow(sup, 3) - 3 * sup));
+
+            return res;
+        }
+
+        private double BoxMuller()
+        {
+            chart1.Series[0].Points.Clear();
+            chart1.Series[1].Points.Clear();
+
+            double a = rnd.NextDouble();
+
+            double res = Math.Sqrt(-2 * Math.Log(a)) * Math.Cos(2 * Math.PI * rnd.NextDouble());
+
+            return res;
+        }
+
+
+
+        private int getIndex(double a_sum)
+        {
+
+            if (a_sum >= -5 && a_sum < -3)
+            {
+                return 0;
+
+            }
+            else if (a_sum >= -3 && a_sum < -2)
+            {
+                return 1;
+
+            }
+            else if (a_sum >= -2 && a_sum < -1)
+            {
+                return 2;
+
+            }
+            else if (a_sum >= -1 && a_sum < 0)
+            {
+
+                return 3;
+            }
+            else if (a_sum >= 0 && a_sum < 1)
+            {
+
+                return 4;
+            }
+            else if (a_sum >= 1 && a_sum < 2)
+            {
+
+                return 5;
+            }
+            else if (a_sum >= 2 && a_sum < 3)
+            {
+
+                return 6;
+            }
+            else if (a_sum >= 3 && a_sum < 5)
+            {
+
+                return 7;
+            }
+
+
+            return -1;
+        }
+    }
+}
